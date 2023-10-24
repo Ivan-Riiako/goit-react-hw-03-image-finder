b5251cca53133ed8da38635cfad2d58866d96a71
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-10-24 19:06:55 +0300

    24.10

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,17 +1,24 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
 import style from './Modal.module.css';
+
+const modalRoot = document.querySelector('#modal-root');
 
  class Modal extends Component {
 
+   
+   componentDidMount() {
+   
+   }
+   
   render() {
-    return (
- 
+  return createPortal(
       <div className={style.Overlay}>
         <div className={style.Modal}>
           {this.props.children}
         </div>
-      </div>
-    )
+      </div>, modalRoot)
+    
   }
 
 }
