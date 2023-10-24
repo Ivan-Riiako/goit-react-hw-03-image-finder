7bd25bd561fdca7243be3f84da7aa797d1f9a4c5
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-10-24 16:29:03 +0300

    24.10

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,9 @@ import style from './App.module.css';
 // const ApiKey = '33947023-c15fa4d03e325678c88d2d925';
 
 class App extends Component {
-  // state = {
-   
-  // };
+  state = {
+    showModal:false,
+  };
 
   render() {
     return (
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -5,10 +5,10 @@ import style from './Searchbar.module.css';
 const Searchbar = ({ onSubmit }) => {
     
         return (
-          <header className={style.Searchbarearchbar}>
+          <header className={style.Searchbar}>
             <form className={style.SearchForm} onClick={onSubmit}>
               <button type="submit" className={style.SearchForm_button}>
-                <span className={style.SearchForm_button_label} c>
+                <span className={style.SearchForm_button_label} >
                   Search
                 </span>
               </button>
@@ -22,8 +22,8 @@ const Searchbar = ({ onSubmit }) => {
               />
             </form>
           </header>
-        );
-    
+  );
+  
 }
 
 
